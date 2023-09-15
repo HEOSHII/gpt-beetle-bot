4c6d2cbb5f0215116ff302302464b732d1ae3853
George Shiryaev <george.shiryaev@gmail.com>
2023-09-15 18:49:05 +0300

    moved bot to root path

--- index.js
+++ index.js
@@ -8,56 +8,56 @@ dotenv.config();
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hello Beetle GPT!');
-});
+	res.send('Beetle GPT alive!');
 
-const bot = new TelegramBot(process.env.API_KEY_BOT, {
-	polling: true,
-});
-
-const openai = new OpenAI({
-	organization: process.env.OPENAI_API_ORGANIZATION,
-	apiKey: process.env.OPENAI_API_KEY,
-});
-
-const getChatAnswer = async ({ content, language = 'en' }) => {
-	const response = await openai.chat.completions.create({
-		model: 'gpt-3.5-turbo-16k',
-		messages: [
-			{
-				role: 'system',
-				content: `Speak in ${language} language`,
-			},
-			{
-				role: 'user',
-				content,
-			},
-		],
+	const bot = new TelegramBot(process.env.API_KEY_BOT, {
+		polling: true,
 	});
 
-	return response.choices[0].message.content;
-};
+	const openai = new OpenAI({
+		organization: process.env.OPENAI_API_ORGANIZATION,
+		apiKey: process.env.OPENAI_API_KEY,
+	});
 
-bot.on('polling_error', err => console.log(err?.data?.error?.message));
-
-bot.on('text', async msg => {
-	try {
-		bot.sendChatAction(msg.chat.id, 'typing');
-
-		const chatAnswer = await getChatAnswer({
-			content: msg.text === '/start' ? `Hello, my name is ${msg.from.first_name}!` : msg.text,
-			language: msg.from.language_code,
+	const getChatAnswer = async ({ content, language = 'en' }) => {
+		const response = await openai.chat.completions.create({
+			model: 'gpt-3.5-turbo-16k',
+			messages: [
+				{
+					role: 'system',
+					content: `Speak in ${language} language`,
+				},
+				{
+					role: 'user',
+					content,
+				},
+			],
 		});
 
-		await bot.sendMessage(msg.chat.id, chatAnswer, {
-			reply_to_message_id: msg.message_id,
-		});
-	} catch (error) {
-		await bot.sendMessage(msg.chat.id, `Sorry, something went wrong! Please, try later.`, {
-			reply_to_message_id: msg.message_id,
-		});
-		console.log(error);
-	}
+		return response.choices[0].message.content;
+	};
+
+	bot.on('polling_error', err => console.log(err?.data?.error?.message));
+
+	bot.on('text', async msg => {
+		try {
+			bot.sendChatAction(msg.chat.id, 'typing');
+
+			const chatAnswer = await getChatAnswer({
+				content: msg.text === '/start' ? `Hello, my name is ${msg.from.first_name}!` : msg.text,
+				language: msg.from.language_code,
+			});
+
+			await bot.sendMessage(msg.chat.id, chatAnswer, {
+				reply_to_message_id: msg.message_id,
+			});
+		} catch (error) {
+			await bot.sendMessage(msg.chat.id, `Sorry, something went wrong! Please, try later.`, {
+				reply_to_message_id: msg.message_id,
+			});
+			console.log(error);
+		}
+	});
 });
 
 const PORT = process.env.PORT || 3000;
